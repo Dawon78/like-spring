66e4737ac3682c5d1205491e7b87b935ca161bcf
Dawon78 <angy7812@naver.com>
2026-08-05 16:24:03 +0900

    Initial commit: Spring Boot + JPA + MySQL project setup

---
+++ src/main/java/like/like_spring/LikeSpringApplication.java
@@ -0,0 +1,15 @@
+package like.like_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeSpringApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/like/like_spring/LikeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package like.like_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
